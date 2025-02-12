0f7e93362c4c2f41545861abc372f646cef3c8e6
tyholland <tyronehus@gmail.com>
2025-02-11 23:11:14 -0600

    test home page render

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import "./App.css";
-import Home from "./pages/home";
+import Home from "./pages/home.tsx";
 
 const App = () => {
   return (

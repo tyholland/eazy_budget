81cfe237014c421cd3f0a01f912d13a694d8a6e7
tyholland <tyronehus@gmail.com>
2025-02-11 22:45:23 -0600

    add some linting, remove test, remove library, remove test from pre-commit

---
+++ .husky/pre-commit
@@ -0,0 +1,2 @@
+npx eslint .
+npx prettier --write src/
--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,16 @@
-import React from 'react';
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-} from "react-router-dom";
-import './App.css';
-import Home from './pages/home';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./App.css";
+import Home from "./pages/home";
 
 const App = () => {
   return (
     <Router>
       <Routes>
-        <Route path='/' element={<Home />} />
+        <Route path="/" element={<Home />} />
       </Routes>
     </Router>
   );
-}
+};
 
 export default App;

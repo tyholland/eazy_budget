b8c0707fdf091f55cafc198ad63aac98f95b01b7
tyholland <tyronehus@gmail.com>
2025-03-05 00:18:30 -0600

    fix overview story

--- src/stories/Overview.stories.ts
+++ src/stories/Overview.stories.ts
@@ -1,8 +1,10 @@
 import Overview from "../views/Overview/Overview.tsx";
+import { withRouter } from "storybook-addon-remix-react-router";
 
 export default {
   title: "Views/Overview",
   component: Overview,
+  decorators: [withRouter],
   parameters: {
     layout: "centered",
   },

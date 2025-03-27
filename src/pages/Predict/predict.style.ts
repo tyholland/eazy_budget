26a55a0835db52c9ca7a8db962f25bf3630856d6
tyholland <tyronehus@gmail.com>
2025-03-26 20:32:52 -0500

    prediction page

--- src/pages/Predict/predict.style.ts
+++ src/pages/Predict/predict.style.ts
@@ -17,6 +17,18 @@ export const PredictInputs = styled.div`
   display: flex;
   gap: 8px;
   flex-direction: column;
+
+  label {
+    @media only screen and (max-width: 800px) {
+      text-align: left;
+    }
+  }
+
+  input {
+    @media only screen and (max-width: 800px) {
+      width: 130px;
+    }
+  }
 `;
 
 export const PredictBudgets = styled.div`
@@ -35,6 +47,11 @@ export const HeaderWrapper = styled.div`
   display: flex;
   justify-content: space-between;
   align-items: flex-start;
+
+  @media only screen and (max-width: 800px) {
+    flex-direction: column;
+    gap: 12px;
+  }
 `;
 
 export const Content = styled.div`
@@ -43,4 +60,8 @@ export const Content = styled.div`
   gap: 12px;
   font-size: 18px;
   width: 40%;
+
+  @media only screen and (max-width: 800px) {
+    width: 100%;
+  }
 `;

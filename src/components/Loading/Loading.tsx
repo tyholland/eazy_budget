6070a6d09a9c163e48baa641cf80d6c9251edf09
tyholland <tyronehus@gmail.com>
2025-04-04 17:33:55 -0500

    add quotes to loading component

--- src/components/Loading/Loading.tsx
+++ src/components/Loading/Loading.tsx
@@ -1,12 +1,17 @@
 import React from "react";
 import LoadingIcon from "../../svg/LoadingIcon.tsx";
 import * as S from "./loading.style.ts";
+import { budgetQuotes } from "../../constants.ts";
 
 const Loading = () => {
+  const numQuotes = budgetQuotes.length;
+  const randomNum = Math.floor(Math.random() * numQuotes);
+
   return (
     <S.Wrapper>
       <LoadingIcon />
       <S.Content>Loading...</S.Content>
+      <div>{budgetQuotes[randomNum]}</div>
     </S.Wrapper>
   );
 };

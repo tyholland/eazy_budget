526102b929e1cf1cbd1e9303245d54261d074ed3
tyholland <tyronehus@gmail.com>
2025-04-08 21:56:50 -0500

    adjust graph/chart height again

--- src/components/Graph/graph.style.ts
+++ src/components/Graph/graph.style.ts
@@ -2,7 +2,9 @@ import styled from "styled-components";
 
 export const GraphWraper = styled.div`
   width: 500px;
-  height: 500px;
+  min-height: 500px;
+  max-height: 1000px;
+  height: 100%;
   margin: 0 auto;
 
   @media only screen and (max-width: 800px) {

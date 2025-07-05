7a3fc87a8ea443664a8d217f5395da79fda00c2e
tyholland <tyronehus@gmail.com>
2025-07-05 16:16:19 -0500

    remove shared access email

--- src/pages/Account/Account.tsx
+++ src/pages/Account/Account.tsx
@@ -306,7 +306,7 @@ const Account = () => {
             >
               <S.ModalWrapper>
                 <span>
-                  Are you sure you want to removed your shared account access?
+                  Are you sure you want to remove shared account access from ${currentUser?.shared_account_email}?
                 </span>
                 <S.ModalBtn>
                   <Button

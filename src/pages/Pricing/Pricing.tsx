ab248af35f868da1a741f2fefd764b93dc318799
tyholland <tyronehus@gmail.com>
2025-08-12 21:49:47 -0500

    remove unused function

--- src/pages/Pricing/Pricing.tsx
+++ src/pages/Pricing/Pricing.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React from "react";
 import * as S from "./pricing.style.ts";
 import PricingDetails from "../../views/PricingDetails/PricingDetails.tsx";
 import { useAtomValue } from "jotai";

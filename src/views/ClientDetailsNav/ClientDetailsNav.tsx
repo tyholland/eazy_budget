2c4df9dc00fa9b356006e3aa92329de79f2f4831
tyholland <tyronehus@gmail.com>
2025-10-25 00:08:28 -0500

    remove variable

--- src/views/ClientDetailsNav/ClientDetailsNav.tsx
+++ src/views/ClientDetailsNav/ClientDetailsNav.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import * as S from "./clientDetailsNav.style.ts";
 import Button from "../../components/Button/Button.tsx";
-import { useNavigate, useParams } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 interface ClientDetailsNavProps {
   setSelectedOption: (val: string) => void;

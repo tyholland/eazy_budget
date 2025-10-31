82060747a1cd53244769d189414e0fed9b632f7d
tyholland <tyronehus@gmail.com>
2025-10-30 23:41:50 -0500

    remove unused variable

--- src/pages/Account/Account.tsx
+++ src/pages/Account/Account.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { useAuth0 } from "@auth0/auth0-react";
 import Button from "../../components/Button/Button.tsx";
 import * as S from "./account.style.ts";

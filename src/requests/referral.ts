bcbd6de3d564a85214accc04ec7971c2d1a2c128
tyholland <tyronehus@gmail.com>
2025-11-01 22:05:20 -0500

    add endpoint for starting trial

--- src/requests/referral.ts
+++ src/requests/referral.ts
@@ -60,3 +60,23 @@ export const getClientBudgetInfo = async (
     throw new Error(`Failed to get client info`);
   }
 };
+
+export const startTrialPlan = async (
+  accessToken: string,
+  updatedSub: Object,
+) => {
+  try {
+    const startTrialResponse = await fetch(`${api}/referral/start`, {
+      method: "PUT",
+      body: JSON.stringify({ ...updatedSub }),
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+        "Content-type": "application/json",
+      },
+    });
+
+    return await startTrialResponse.json();
+  } catch (err) {
+    throw new Error(`Failed to start referral plan`);
+  }
+};

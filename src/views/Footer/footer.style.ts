a43a9c5f8ef4854765f23871923e32f2e8c12381
tyholland <tyronehus@gmail.com>
2025-12-28 22:22:00 -0600

    fix footer height

--- src/views/Footer/footer.style.ts
+++ src/views/Footer/footer.style.ts
@@ -9,7 +9,7 @@ export const Wrapper = styled.div`
   position: absolute;
   bottom: 0;
   width: 100%;
-  height: 120px;
+  height: 146px;
 
   @media only screen and (max-width: 800px) {
     margin-bottom: 10px;

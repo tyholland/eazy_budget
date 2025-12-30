b9df77b02f65605f04dd087a43451853d2c9048d
tyholland <tyronehus@gmail.com>
2025-12-29 21:48:08 -0600

    remove manual upload

--- src/components/Breadcrumb/Breadcrumb.tsx
+++ src/components/Breadcrumb/Breadcrumb.tsx
@@ -45,7 +45,7 @@ const Breadcrumb = () => {
         )}
         <div>{pageName}</div>
       </S.Wrapper>
-      {currentUser && !currentUser.hasBudget && (
+      {currentUser && !currentUser.hasBudget && isAccount && (
         <BannerBtn url="/setup" label="Add your Budget info!!!" />
       )}
     </>
